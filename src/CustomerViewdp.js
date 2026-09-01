360a374f6223f5172f118af8309e90e01034e22c
fathimasahna <fathimasahna980@gmail.com>
2026-09-01 21:35:11 +0530

    last check

--- src/CustomerViewdp.js
+++ src/CustomerViewdp.js
@@ -1,44 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import d1 from './CustomerViewdp.module.css'
-import Navbar from './Navbar'
+import d1 from './CustomerViewdp.module.css';
+import Navbar from './Navbar';
+
 const CustomerViewdp = () => {
   const [orders, setOrders] = useState([]);
-useEffect(() => {
-  axios.get("https://e-commerce-application-fdjs.onrender.com/sample/complaintGet")
-    .then((response) => {
-      console.log(response.data);
-      setOrders(response.data);
-    })
-    .catch((error) => console.error(error));
-}, []);
 
+  useEffect(() => {
+  
+axios.get("https://e-commerce-application-fdjs.onrender.com/sample/complaintGet/")      .then((response) => {
+        console.log(response.data);
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching complaints:", error);
+      });
+  }, []);
 
   return (
-  <div>
-<Navbar/>
-    <div className={d1.outer}>
-        
-      <table border={1} className={d1.table}>
-        <thead>
-          <tr>
-            <th>Product Name</th>
-            <th>Complaint</th>
-            <th>Complaint Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((item) => (
-            <tr key={item.complaint_id}>
-              <td>{item.product_name}</td>
-              <td>{item.complaints}</td>
-              <td>{item.complaint_date}</td>
+    <div>
+      <Navbar />
+
+      <div className={d1.outer}>
+        <table border={1} className={d1.table}>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Complaint</th>
+              <th>Complaint Date</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {orders.map((item) => (
+              <tr key={item.complaint_id}>
+                <td>{item.product_name}</td>
+                <td>{item.complaints}</td>
+                <td>{item.complaint_date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>   
   );
 };
 
