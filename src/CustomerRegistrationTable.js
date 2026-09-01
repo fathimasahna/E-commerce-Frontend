fa7de4f7a67d019e3c8c16d2eff6a847e2aabb55
fathimasahna <fathimasahna980@gmail.com>
2026-09-01 14:02:40 +0530

    cust reg table fixed

--- src/CustomerRegistrationTable.js
+++ src/CustomerRegistrationTable.js
@@ -1,41 +1,51 @@
-import React from 'react'
-import { useState ,useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const CustomerRegistrationTable = () => {
-    const[table,setTable]=useState({})
-     useEffect(() => {
+
+  const [table, setTable] = useState([]);
+
+  useEffect(() => {
     axios
       .get("https://e-commerce-application-fdjs.onrender.com/sample/CustomerGet/")
-      .then((response) => setTable(response.data));
-       
-  }, [])
+      .then((response) => {
+        console.log("Customer data:", response.data);
+        setTable(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching customers:", error);
+      });
+
+  }, []);
+
   return (
     <div>
       <table>
         <thead>
-            <tr>
-                <th>ID</th>
-                <th>Customer NAME</th>
-                <th>Phone no</th>
-                <th>email</th>
-                <th>Address</th>
-            </tr>
+          <tr>
+            <th>ID</th>
+            <th>Customer NAME</th>
+            <th>Phone no</th>
+            <th>Email</th>
+            <th>Address</th>
+          </tr>
         </thead>
+
         <tbody>
           {table.map((item) => (
-            <tr key={item.shopId}>
+            <tr key={item.id}>
+              <td>{item.id}</td>
               <td>{item.name}</td>
               <td>{item.phoneNo}</td>
               <td>{item.email}</td>
               <td>{item.address}</td>
-
-              
             </tr>
           ))}
         </tbody>
+
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default CustomerRegistrationTable
+export default CustomerRegistrationTable;
